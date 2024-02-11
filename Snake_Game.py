02d8f90808b577a73ebc27a4b714e0ff837d78e2
William Owen <98859927+William0wen@users.noreply.github.com>
2024-02-12 09:52:17 +1300

    snake update

--- Snake_Game.py
+++ Snake_Game.py
@@ -1,5 +1,18 @@
 import pygame
+import time
 pygame.init()
+
+
+screen = pygame.display.set_mode((1000, 720))
+game_icon = pygame.image.load("snake_icon.png")
+pygame.display.set_icon(game_icon)
+pygame.display.set_caption("Snake Game")
+
+quit_game = False
+while not quit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit_game = True
 
 
 pygame.quit()
