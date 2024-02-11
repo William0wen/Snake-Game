e375712c1f6b5ad686311d513611c1c243f0d6ce
William Owen <98859927+William0wen@users.noreply.github.com>
2024-02-12 10:05:14 +1300

    Update Snake_Game.py

--- Snake_Game.py
+++ Snake_Game.py
@@ -18,9 +18,14 @@ green = (188, 227, 119)
 score_font = pygame.font.SysFont("consolas", 20)
 exit_font = pygame.font.SysFont("arialblack", 30)
 
+clock = pygame.time.Clock()  # sets the frame rate
+
 # snake will be 20 * 20px
 snake_x = 490  # centre point horizontally is (1000-20 snake width)/2 =490
 snake_y = 350
+
+snake_x_change = 0  # holds the value of changes in the x co-ord
+snake_y_change = 0
 
 
 quit_game = False
@@ -28,6 +33,8 @@ while not quit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit_game = True
+        if event.type == pygame.KEYDOWN:
+            if event.key
 
     # create rect for snake
     pygame.draw.rect(screen, red, [snake_x, snake_y, 20, 20])
