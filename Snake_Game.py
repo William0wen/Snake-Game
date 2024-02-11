342c228c80048fd9a91622d1aca6b2d4b630fbea
William Owen <98859927+William0wen@users.noreply.github.com>
2024-02-12 09:58:44 +1300

    Update Snake_Game.py

--- Snake_Game.py
+++ Snake_Game.py
@@ -8,6 +8,17 @@ game_icon = pygame.image.load("snake_icon.png")
 pygame.display.set_icon(game_icon)
 pygame.display.set_caption("Snake Game")
 
+# colour variables
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (188, 227, 119)
+
+# game fonts
+score_font = pygame.font.SysFont("consolas", 20)
+exit_font = pygame.font.SysFont("arialblack", 30)
+
+
 quit_game = False
 while not quit_game:
     for event in pygame.event.get():
