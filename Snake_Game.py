2e67fe58542d6ecd56a6c5426b28baa261e505cf
William Owen <98859927+William0wen@users.noreply.github.com>
2024-02-13 11:59:03 +1300

    Update Snake_Game.py

--- Snake_Game.py
+++ Snake_Game.py
@@ -33,12 +33,31 @@ while not quit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit_game = True
+
         if event.type == pygame.KEYDOWN:
-            if event.key
+            if event.key == pygame.K_LEFT:
+                snake_x_change = -20
+                snake_y_change = 0
+            elif event.key == pygame.K_RIGHT:
+                snake_x_change = 20
+                snake_y_change = 0
+            elif event.key == pygame.K_UP:
+                snake_x_change = 0
+                snake_y_change = -20
+            elif event.key == pygame.K_DOWN:
+                snake_x_change = 0
+                snake_y_change = 20
+
+    snake_x += snake_x_change
+    snake_y += snake_y_change
+
+    screen.fill(green)
 
     # create rect for snake
     pygame.draw.rect(screen, red, [snake_x, snake_y, 20, 20])
     pygame.display.update()
+
+    clock.tick(15)
 
 
 pygame.quit()
